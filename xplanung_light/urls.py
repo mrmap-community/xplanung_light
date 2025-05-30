fa18eac558c073fa897da8db210a1758abc141cd
armin11 <armin.retterath@gmail.com>
2025-05-30 08:20:19 +0200

    Standardfunktionen für Registrierung und Anmeldung hinzufügen

--- xplanung_light/urls.py
+++ xplanung_light/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from xplanung_light import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("accounts/login/", auth_views.LoginView.as_view(next_page="home"), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(next_page="home"), name='logout'),
+    # https://dev.to/donesrom/how-to-set-up-django-built-in-registration-in-2023-41hg
+    path("register/", views.register, name = "register"),
+    path("about/", views.about, name="about"),
 ]
--- xplanung_light/views.py
+++ xplanung_light/views.py
@@ -1,4 +1,27 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from xplanung_light.forms import RegistrationForm
+from django.shortcuts import redirect
+from django.contrib.auth import login
 
 def home(request):
-    return HttpResponse("Hello, XPlanung!")
+    return render(request, "xplanung_light/home.html")
+    
+def about(request):
+    return render(request, "xplanung_light/about.html")
+
+# https://dev.to/balt1794/registration-page-using-usercreationform-django-part-1-21j7
+def register(request):
+    if request.method != 'POST':
+        form = RegistrationForm()
+    else:
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            print('form is invalid')
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
