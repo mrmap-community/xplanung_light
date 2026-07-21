21a623d822cb8d2271e75d28ab23c7a31dc25fda
armin11 <armin.retterath@gmail.com>
2026-07-21 12:04:27 +0200

    Leichte Anpassung mit debug Ausgabe

--- komserv/settings/__init__.py
+++ komserv/settings/__init__.py
@@ -7,13 +7,16 @@ CONFIG_FILE = BASE_DIR / '.my_xplanung_light_env.json'
 
 from .base import *
 
-try:
+
+if CONFIG_FILE.exists():
     with open(CONFIG_FILE) as config_file:
         config = json.load(config_file)
-        if config['ENV_NAME'] == 'dev':
+        env = config.get('ENV_NAME')
+        
+        if env == 'dev':
             from .dev import *
-        if config['ENV_NAME'] == 'prod':
+        elif env == 'prod':
             from .prod import *
-except FileNotFoundError:
-    # If JSON is missing, it safely falls back to base settings
-    pass
+else:
+    # Hilft beim Debuggen unter Gunicorn: Zeigt im Log, wo gesucht wurde
+    print(f"WARNUNG: Umgebungskonfiguration nicht gefunden unter: {CONFIG_FILE}")
